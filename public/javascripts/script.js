fbed042836bbcb8b63fa460c34e14989654eeb32
andrei <andrei.mihailescu994@gmail.com>
2019-07-14 23:40:39 +0300

    Adding socket.io

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,31 @@
 var App = {
     videoPlayerNode: document.getElementById('videoPlayer'),
+    socket: io(),
 
     getCurrentTime() {
         return this.videoPlayerNode.currentTime;
+    },
+
+    sendEvent() {
+        this.socket.emit('event', 'WORKS');
+    },
+
+    init() {
+        var self = this;
+
+        this.videoPlayerNode.onplay = function () {
+            self.socket.emit('event', 'Video started');
+        };
+
+        this.videoPlayerNode.onpause = function () {
+            self.socket.emit('event', 'Video paused');
+        };
+
+        this.socket.onevent = function (data) {
+            console.log('EVENT', data);
+            alert('WORKS');
+        };
     }
 };
+
+App.init();
